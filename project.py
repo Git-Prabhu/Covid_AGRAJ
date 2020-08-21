20b37301f5ff855a14376d35cae4aec9f2924021
Git-Prabhu <pcube@hotmail.com>
2020-08-21 18:33:12 -0400

    Latest version of project

--- project.py
+++ project.py
@@ -7,7 +7,13 @@ from PIL import Image
 import custom_gauge as cg
 
 image = Image.open('COVID_Agraj_image.jpg')
-st.image (image, caption='A COVID-19 Predictor', use_column_width=True)
+st.image (image, caption='A COVID-19 Mortality Risk Predictor', use_column_width=True)
+
+st.sidebar.header ("Select your inputs")
+#st.sidebar.subheader ("Select your inputs")
+
+st.sidebar.text ("")
+#st.sidebar.text ("")
 
 gender = st.sidebar.selectbox("Select Gender",("Male", "Female"))
 age = st.sidebar.selectbox("Select Age group",("0-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75-84", "Above 85"))
@@ -18,9 +24,15 @@ state = st.sidebar.selectbox("Select your state",("Alabama", "Alaska", "Arizona"
             "Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"))
 st.sidebar.text ("")
 st.sidebar.text ("")
-NPI1 = st.sidebar.checkbox ("I follow all CDC guidlines (Practice social distancing, Mandatory Mask-wearing in public spaces, Lockdown (Staying at home unless otherwise necessary))")
-#NPI2 = st.sidebar.checkbox ("I practice social distancing as per CDC Guidelines")
-#NPI3 = st.sidebar.checkbox ("I use face coverings as per CDC Guidelines")
+
+st.sidebar.text ("Does your state do the following:")
+NPI1 = st.sidebar.checkbox ("Practice social distancing?")
+NPI2 = st.sidebar.checkbox ("Mandatory Mask-wearing in public spaces?")
+NPI3 = st.sidebar.checkbox ("School closures?")
+NPI4 = st.sidebar.checkbox ("Mass gathering restrictions?")
+NPI5 = st.sidebar.checkbox ("Non-essential business closures?")
+NPI6 = st.sidebar.checkbox ("Stay at home orders (with exemptions)?")
+NPI7 = st.sidebar.checkbox ("Measures to isolate symptomatic individuals and their contacts?")
 
 st.sidebar.text ("")
 st.sidebar.text ("")
@@ -30,9 +42,13 @@ def user_input_features():
                 'age': age,
                 'race': race,
                 'state': state,
-                'NPI1': NPI1}
-                #'NPI2': NPI2,
-                #'NPI3': NPI3}
+                'NPI1': NPI1,
+                'NPI2': NPI2,
+                'NPI3': NPI3,
+                'NPI4': NPI4,
+                'NPI5': NPI5,
+                'NPI6': NPI6,
+                'NPI7': NPI7}
         features = pd.DataFrame (data,index=[0])
         return features
 
@@ -90,10 +106,10 @@ output_df4 = output_age()
 
 def output_NPI():
         data = None
-        if NPI1 == 1:
+        if NPI1 == 1 & NPI2 == 1 & NPI3 == 1 & NPI4 == 1 & NPI5 == 1 & NPI6 == 1 & NPI7 == 1:
             data = 0.18
         else:
-            data = 0.82
+            data = 1
         b = data
         return b
 output_df5 = output_NPI()
@@ -205,8 +221,7 @@ def output_state():
 output_df6 = output_state()
 
 def aggregate_calc():
-    #data = ((output_df1*0.25)+(output_df2*0.25)+(output_df4*0.25)+(output_df6*0.25))/4*(output_df5)
-    data = ((output_df1+output_df2+output_df4+output_df6)*output_df5)
+    data = (((output_df1+output_df2+output_df4+output_df6)/4)*output_df5)
     z = data
     return z
 output_df3 = aggregate_calc()
@@ -227,7 +242,7 @@ st.write("""
 if st.sidebar.button('Submit'):
         cg.render_gauge((int(output_df3)))
         #st.write(cg.render_gauge((int(output_df3))))
-
+        st.write("Please continue to social distance and wear a mask in public. Keep at least 6 feet between you and others at all times. Do not be within contact for longer than 10 minutes. If you experience any symptoms, please isolate yourself and get tested as soon as possible.")
 #cg.render_gauge((int(output_df3)))
 
 fig = go.Figure(go.Indicator(
